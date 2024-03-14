3517eea296997771677951f25ae69cc0c6777d7b
Kirillov_Ivan <kirillovio@mail.ru>
2024-03-14 19:34:20 +0300

    add test_init_category

---
+++ tests/test_class.py
@@ -0,0 +1,54 @@
+import pytest
+
+from ../src import class_
+
+
+@pytest.fixture()
+def class_smart():
+    return class_.Category("Смартфоны",
+    "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни",
+    [
+      {
+        "name": "Samsung Galaxy C23 Ultra",
+        "description": "256GB, Серый цвет, 200MP камера",
+        "price": 180000.0,
+        "quantity": 5
+      },
+      {
+        "name": "Iphone 15",
+        "description": "512GB, Gray space",
+        "price": 210000.0,
+        "quantity": 8
+      },
+      {
+        "name": "Xiaomi Redmi Note 11",
+        "description": "1024GB, Синий",
+        "price": 31000.0,
+        "quantity": 14
+      }
+    ])
+
+
+def test_init_category(class_smart):
+  assert class_smart.name == "Смартфоны"
+  assert class_smart.description == "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни"
+  assert class_smart.products == [
+      {
+        "name": "Samsung Galaxy C23 Ultra",
+        "description": "256GB, Серый цвет, 200MP камера",
+        "price": 180000.0,
+        "quantity": 5
+      },
+      {
+        "name": "Iphone 15",
+        "description": "512GB, Gray space",
+        "price": 210000.0,
+        "quantity": 8
+      },
+      {
+        "name": "Xiaomi Redmi Note 11",
+        "description": "1024GB, Синий",
+        "price": 31000.0,
+        "quantity": 14
+      }
+    ]
