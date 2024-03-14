b2fec9df11b5ea2610f5ee793996e9701c921898
Kirillov_Ivan <kirillovio@mail.ru>
2024-03-14 20:01:30 +0300

    add test_init_products

--- tests/test_class.py
+++ tests/test_class.py
@@ -28,7 +28,9 @@ def class_smart():
       }
     ])
 
-
+@pytest.fixture()
+def class_products():
+    return class_.Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
 def test_init_category(class_smart):
   assert class_smart.name == "Смартфоны"
   assert class_smart.description == "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни"
@@ -52,3 +54,9 @@ def test_init_category(class_smart):
         "quantity": 14
       }
     ]
+
+def test_init_products(class_products):
+    assert class_products.name = "Samsung Galaxy C23 Ultra"
+    assert class_products.description = "256GB, Серый цвет, 200MP камера"
+    assert class_products.price = 180000.0
+    assert class_products.quantity = 5
