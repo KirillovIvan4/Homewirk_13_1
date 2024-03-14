cf7bae12d5cf7d0cd33d2a7b90bd99a1be7c0e78
Kirillov_Ivan <kirillovio@mail.ru>
2024-03-14 19:17:39 +0300

    structure homework

---
+++ utils/utils.py
@@ -0,0 +1,11 @@
+import json
+
+
+def get_list():
+    """Функция считывает json файл преобразует его список"""
+    with open('../products.json', 'r', encoding='utf-8') as json_data_products:
+        data_products = json.load(json_data_products)
+        return data_products
+
+for i in get_list():
+    print(i)
