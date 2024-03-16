dd434c4a229d64ed58e7e98e22789019f1c5f3a0
Kirillov_Ivan <kirillovio@mail.ru>
2024-03-16 18:16:33 +0300

    fix test_class.py fix class.py

--- tests/test_class.py
+++ tests/test_class.py
@@ -2,58 +2,30 @@ import pytest
 
 from src import class_
 
-
-@pytest.fixture()
-def class_smart():
-    return class_.Category("Смартфоны",
-    "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни",
-    [
-      {
-        "name": "Samsung Galaxy C23 Ultra",
-        "description": "256GB, Серый цвет, 200MP камера",
-        "price": 180000.0,
-        "quantity": 5
-      },
-      {
-        "name": "Iphone 15",
-        "description": "512GB, Gray space",
-        "price": 210000.0,
-        "quantity": 8
-      },
-      {
-        "name": "Xiaomi Redmi Note 11",
-        "description": "1024GB, Синий",
-        "price": 31000.0,
-        "quantity": 14
-      }
-    ])
-
 @pytest.fixture()
 def class_products():
     return class_.Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+
+
+@pytest.fixture()
+def class_smart(class_products):
+    return class_.Category("Смартфоны",
+    "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни",
+    [class_products])
+
+
 def test_init_category(class_smart):
   assert class_smart.name == "Смартфоны"
   assert class_smart.description == "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни"
-  assert class_smart.products == [
-      {
-        "name": "Samsung Galaxy C23 Ultra",
-        "description": "256GB, Серый цвет, 200MP камера",
-        "price": 180000.0,
-        "quantity": 5
-      },
-      {
-        "name": "Iphone 15",
-        "description": "512GB, Gray space",
-        "price": 210000.0,
-        "quantity": 8
-      },
-      {
-        "name": "Xiaomi Redmi Note 11",
-        "description": "1024GB, Синий",
-        "price": 31000.0,
-        "quantity": 14
-      }
-    ]
+  assert class_smart.products[0].name == 'Samsung Galaxy C23 Ultra'
+
+
+def test_nummer_of_category(class_smart):
+    assert  class_smart.nummer_of_category == 3
+
+
+def test_nummer_of_products(class_smart):
+    assert  class_smart.nummer_of_products == 1
 
 
 def test_init_products(class_products):
