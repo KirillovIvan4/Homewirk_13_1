fb2ba5ff0e1da25386a5a422f14c76e48e238841
Kirillov_Ivan <kirillovio@mail.ru>
2024-03-19 06:00:59 +0300

    fix Category

--- src/main.py
+++ src/main.py
@@ -12,6 +12,6 @@ for i in range (len(data_category)):
         product = class_Product.Product(path_product['name'], path_product['description'], path_product['price'], path_product['quantity'])
         list_products.append(product)
 
-    category = class_Category.Category(data_category[i]['name'], data_category[i]['description'], data_category[i]['products'])
+    category = class_Category.Category(data_category[i]['name'], data_category[i]['description'], list_products)
     list_category.append(category)
 print(list_category)
