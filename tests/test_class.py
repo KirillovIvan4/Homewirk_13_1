5247510ede84dd7b6480d172b304bf9531781d8c
Kirillov_Ivan <kirillovio@mail.ru>
2024-03-22 06:17:21 +0300

    fix class

--- tests/test_class.py
+++ tests/test_class.py
@@ -1,19 +1,19 @@
 import pytest
 from io import StringIO
 
-from src import class_Category,class_Product
+from src import class_category,class_product
 
 @pytest.fixture()
 def class_products_1():
-    return class_Product.Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    return class_product.Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
 
 @pytest.fixture()
 def class_products_2():
-    return class_Product.Product("Iphone 15", "512GB, Gray space", 210000.0, 25)
+    return class_product.Product("Iphone 15", "512GB, Gray space", 210000.0, 25)
 
 @pytest.fixture()
 def class_smart(class_products_1):
-    return class_Category.Category("Смартфоны",
+    return class_category.Category("Смартфоны",
     "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни",
     [class_products_1])
 
@@ -21,7 +21,7 @@ def class_smart(class_products_1):
 def test_init_category(class_smart):
   assert class_smart.name == "Смартфоны"
   assert class_smart.description == "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни"
-  assert class_smart.add_product[0].name == "Samsung Galaxy C23 Ultra"
+  assert class_smart.product[0].name == "Samsung Galaxy C23 Ultra"
 
 
 def test_nummer_of_category(class_smart):
@@ -38,21 +38,20 @@ def test_init_products(class_products_1):
     assert class_products_1.price == 180000.0
     assert class_products_1.quantity == 5
 
-def test_add_product(class_smart):
-    class_smart.add_product = class_products_1
-
-    assert class_smart.add_product[0].name == ["Samsung Galaxy C23 Ultra", "Iphone 15"]
+# def test_add_product(class_smart):
+#     class_smart.product = class_products_1
+#     assert class_smart.product() == ["Samsung Galaxy C23 Ultra", "Iphone 15"]
 
 def test_get_product_and_price_and_quantity(class_smart):
-    assert class_smart.get_product_and_price_and_quantity == "Samsung Galaxy C23 Ultra, 180000.0 руб. Остаток: 5 шт \n"
+    assert class_smart.product_and_price_and_quantity == ["Samsung Galaxy C23 Ultra, 180000.0 руб. Остаток: 5 шт"]
 
-def test_get_new_price(class_products_1):
-    class_products_1.get_new_price = 580000
-    assert class_products_1.get_new_price == 580000
-    class_products_1.get_new_price = -10
-    assert class_products_1.get_new_price == 'Цена введена некорректная'
-    class_products_1.get_new_price = 0
-    assert class_products_1.get_new_price == 'Цена введена некорректная'
+def test_new_price(class_products_1):
+    class_products_1.new_price = 580000
+    assert class_products_1.new_price == 580000
+    class_products_1.new_price = -10
+    assert class_products_1.new_price == 580000
+    class_products_1.new_price = 0
+    assert class_products_1.new_price == 580000
     # Ни как не разобраться как протестировать input
     # class_products_1.get_new_price = 10
     # assert class_products_1.get_new_price == 180000
