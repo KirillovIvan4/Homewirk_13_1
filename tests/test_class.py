bc5becbaa5408d1eec0bb1bde2db9ae2b934717f
Kirillov_Ivan <kirillovio@mail.ru>
2024-03-23 16:41:28 +0300

    fix class_product fix test

--- tests/test_class.py
+++ tests/test_class.py
@@ -17,11 +17,11 @@ def class_smart(class_products_1):
     "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни",
     [class_products_1])
 
-
 def test_init_category(class_smart):
   assert class_smart.name == "Смартфоны"
   assert class_smart.description == "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни"
   assert class_smart.product[0].name == "Samsung Galaxy C23 Ultra"
+
 
 
 def test_nummer_of_category(class_smart):
@@ -38,12 +38,21 @@ def test_init_products(class_products_1):
     assert class_products_1.price == 180000.0
     assert class_products_1.quantity == 5
 
+
 # def test_add_product(class_smart):
 #     class_smart.product = class_products_1
 #     assert class_smart.product() == ["Samsung Galaxy C23 Ultra", "Iphone 15"]
 
 def test_get_product_and_price_and_quantity(class_smart):
     assert class_smart.product_and_price_and_quantity == ["Samsung Galaxy C23 Ultra, 180000.0 руб. Остаток: 5 шт"]
+
+
+def test_from_string(class_smart):
+    test_new_product = class_product.Product.from_string("test_smart description 100 1")
+    assert test_new_product.name == "test_smart"
+    assert test_new_product.description == "description"
+    assert test_new_product.price == 100.0
+    assert test_new_product.quantity == 1
 
 def test_new_price(class_products_1):
     class_products_1.new_price = 580000
